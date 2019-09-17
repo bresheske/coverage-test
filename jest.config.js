5313bc1e1ec43ff0a6d086c90cdea5550fb06c45
brandon resheske <brandon.resheske@tasconline.com>
2019-09-17 09:08:09 -0500

    working builds

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "testEnvironment": "node",
+    "testRegex": [
+        "\\.spec\\.js$"
+    ],
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "src/**/*.js"
+    ],
+    "coverageReporters": [
+        "text-lcov"
+    ]
+}
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,6 +2,7 @@ import * as mathFuncs from './index';
 
 describe(`index tests`, () => {
     it(`should add`, () => {
-        
+        const result = mathFuncs.add(1, 1);
+        expect(result).toBe(2);
     });
 });
